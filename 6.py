17315b35c8bfe568ace05812149c39036c03fce4
sreesubhash <snehasubhash964@gmail.com>
2022-05-08 14:20:39 +0530

    First commit

---
+++ 6.py
@@ -0,0 +1,9 @@
+r = 6
+
+A = 3.14 * (r)**2  # Area of circle = pi * r**2 i.e. (pi = 3.14)
+
+C = 2 * 3.14 * r  # Circuference of circle = 2 * pi * r
+
+print(f'area of circle {r} is {A}')
+
+print(f'circumference of circle {r} is {C}')
